a522441c86e65290d0f762516fc14214e8a08617
Ron Braha <Ron.Braha@icloud.com>
2020-09-11 17:57:58 +0300

    fix geolocation

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useLocation } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 
 import Routes from './routes/Routes'
 import Layout from './components/layout/Layout'
 import { geoPosition, search } from './store/actions/app'
-import { RootState } from './store/reducer/app'
 
 function App() {
 	const dispatch = useDispatch()
-	const { pathname } = useLocation()
-	const { currentCity } = useSelector((state: RootState) => state)
+
 	useEffect(() => {
-		if (pathname === '/' && !currentCity)
-			navigator.geolocation.getCurrentPosition(
-				({ coords: { latitude, longitude } }) => {
-					const coords = { lat: latitude.toString(), lon: longitude.toString() }
-					dispatch(geoPosition(coords))
-				},
-				(_: PositionError) =>
-					dispatch(search({ name: 'Tel Aviv', locationKey: '215854' })),
-				{ enableHighAccuracy: true }
-			)
-	}, [dispatch, pathname, currentCity])
+		navigator.geolocation.getCurrentPosition(
+			({ coords: { latitude, longitude } }) => {
+				const coords = { lat: latitude.toString(), lon: longitude.toString() }
+				dispatch(geoPosition(coords))
+			},
+			(_: PositionError) =>
+				dispatch(search({ name: 'Tel Aviv', locationKey: '215854' })),
+			{ enableHighAccuracy: true }
+		)
+	}, [dispatch])
 	return (
 		<Layout>
 			<Routes />
