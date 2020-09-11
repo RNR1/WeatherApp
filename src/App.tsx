d1702dc5d5fecfc87388f1a7c846f709bff3e26f
Ron Braha <Ron.Braha@icloud.com>
2020-09-11 08:53:02 +0300

    use geolocation only on homepage

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,23 @@ import Routes from './routes/Routes'
 import Layout from './components/Layout'
 import { useDispatch } from 'react-redux'
 import { geoPosition, search } from './store/actions/app'
+import { useLocation } from 'react-router-dom'
 
 function App() {
 	const dispatch = useDispatch()
+	const { pathname } = useLocation()
 	useEffect(() => {
-		navigator.geolocation.getCurrentPosition(
-			({ coords: { latitude, longitude } }) => {
-				const coords = { lat: latitude.toString(), lon: longitude.toString() }
-				dispatch(geoPosition(coords))
-			},
-			(error) => dispatch(search({ name: 'Tel Aviv', locationKey: '215854' })),
-			{ enableHighAccuracy: true }
-		)
-	}, [dispatch])
+		if (pathname === '/')
+			navigator.geolocation.getCurrentPosition(
+				({ coords: { latitude, longitude } }) => {
+					const coords = { lat: latitude.toString(), lon: longitude.toString() }
+					dispatch(geoPosition(coords))
+				},
+				(error) =>
+					dispatch(search({ name: 'Tel Aviv', locationKey: '215854' })),
+				{ enableHighAccuracy: true }
+			)
+	}, [dispatch, pathname])
 	return (
 		<Layout>
 			<Routes />
